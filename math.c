4e0b1530ef6f800be346fd5ad1936b349abd64e3
Ana Carolina Bras Costa <P13243094@GH582-011.lec-admin.dmu.ac.uk>
2015-01-29 16:39:26 +0000

    SEgunda versao do algoritmo

--- math.c
+++ math.c
@@ -31,4 +31,25 @@ int main()
 
             printf("Calculando: %.2f %c %.2f = ", num1,oper,num2);
 
+switch( oper )
+            {
+                case '+':
+                        printf("%.2f\n\n", num1 + num2);
+                        break;
+
+                case '-':
+                        printf("%.2f\n\n", num1 - num2);
+                        break;
+
+                case '*':
+                        printf("%.2f\n\n", num1 * num2);
+                        break;
+
+                case '/':
+                        if(num2 != 0)
+                            printf("%.2f\n\n", num1 / num2);
+                        else
+                            printf("Nao existe divisao por 0\n\n");
+                        break;
+
 
