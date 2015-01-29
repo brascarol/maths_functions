a2402c315fe8eb5a48dca46809654a9482d148e5
Ana Carolina Bras Costa <P13243094@GH582-011.lec-admin.dmu.ac.uk>
2015-01-29 16:36:36 +0000

    Primeira versao do algoritmo da calculadora

---
+++ math.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+        float num1,
+              num2;
+              
+        char op;
+
+        do
+        {
+            printf("\t\tCalculadora do curso C Progressivo\n\n");
+
+            printf("Operacoes disponiveis\n");
+            printf("'+' : soma\n");
+            printf("'-' : subtracao\n");
+            printf("'*' : multiplicao\n");
+            printf("'/' : divisao\n");
+            printf("'%%' : resto da divisao\n");
+
+            printf("\nDigite a expressao na forma: numero1 operador numero2\n");
+            printf("Exemplos: 1 + 1 ,  2.1 * 3.1\n");
+            printf("Para sair digite: 0 0 0\n");
+
+
+            scanf("%f", &num1);
+            scanf(" %c",&op);
+            scanf("%f", &num2);
+
+            system("cls || clear");
+
+            printf("Calculando: %.2f %c %.2f = ", num1,oper,num2);
+
+
